18292febca5fc5767363f88b8cf7f7a791a0848c
Mipan-Zuzu <kadekisaka00@gmail.com>
2025-11-02 23:06:05 +0800

    nested component useCase

--- src/Components/Fragments/CardProduct.jsx
+++ src/Components/Fragments/CardProduct.jsx
@@ -1,83 +1,66 @@
-import Button from "../Elements/Button/index";
+import Button from '../Elements/Button/index'
 
 const CardProduct = (props) => {
-  const { img, title, detailShort, detailLong } = props;
-  return (
-    <div className="flex justify-self-center items-center min-h-screen hover:scale-95 duration-300">
-      <div className="w-full max-w-sm bg-white border border-gray600 rounded-r-2xl shadow">
+    const {children} = props
+    return (
+      <div className="w-full max-w-sm bg-white border border-gray-600 rounded-r-2xl shadow hover:scale-95 duration-300">
+        {children}
+      </div>
+    )
+}
+
+const Header = (props) => {
+    const {img} = props
+    return (
         <a href="#">
-          <ImgCard img={img} />
+          <img
+            src={`/images/${img}`}
+            alt="produc"
+            className="p-8 rounded-t-lg"
+          />
         </a>
-        <div className="px pb-5">
+    )
+}
+
+const Body = (props) => {
+    const {children, title, detail} = props
+    return (
+             <div className="px pb-5">
           <a href="">
             <div className="flex gap-2 mb-3 justify-self-center">
-              <Dots bg={"bg-black"} />
-              <Dots bg={"bg-white"} />
-              <Dots bg={"bg-cyan-400"} />
+              <a className="p-2 h-2 w-2 border border-black rounded-full bg-black inline-block"></a>
+              <a className="p-2 h-2 w-2 border border-black rounded-full bg-cyan-700 inline-block"></a>
+              <a className="p-2 h-2 w-2 rounded-full border border-gray-500 inline-block"></a>
             </div>
-            <Title>{title}</Title>
-            <DetailShort>{detailShort}</DetailShort>
-            <DetailLong>
-              {detailLong}
-            </DetailLong>
+            <h3 className="text-2xl text-center font-mono font-semibold tracking-tight">
+                {title}
+            </h3>
+            <p className="text-center font-medium font-mono">
+              {detail}
+            </p>
+            <p className="text-sm max-w-[300px] justify-self-center font-mono text-center text-gray-500">
+              {children}
+            </p>
           </a>
         </div>
+    )
+}
+
+const Footer = () => {
+    return (
         <div className="flex gap-3 items-center mb-10 justify-self-center justify-between">
-          <Selengkap />
+          <span className="rounded-2xl font-mono p-2 bg-blue-500 text-white">
+            Selengkapnya
+          </span>
           <Button variant={"bg-black"} color={"text-white"}>
             Buy ›
           </Button>
         </div>
-      </div>
-    </div>
-  );
-};
+    )
+}
 
-export const ImgCard = (props) => {
-  const { img } = props;
-  return (
-    <img src={`/images/${img}`}  className="p-8 rounded-t-lg" />
-  );
-};
+CardProduct.Header = Header
+CardProduct.Body = Body
+CardProduct.Footer = Footer
 
-export const Dots = (props) => {
-  const { bg } = props;
-  return (
-    <a
-      className={`p-2 h-2 w-2 border border-black rounded-full ${bg} inline-block`}
-    ></a>
-  );
-};
-
-export const Title = (props) => {
-  const { children } = props;
-  return (
-    <h3 className="text-2xl text-center font-mono font-semibold tracking-tight">
-      {children}
-    </h3>
-  );
-};
-
-export const DetailShort = (props) => {
-  const { children } = props;
-  return <p className="text-center font-medium font-mono">{children}</p>;
-};
-
-export const DetailLong = (props) => {
-  const { children } = props;
-  return (
-    <p className="text-sm max-w-[300px] justify-self-center font-mono text-center text-gray-500">
-      {children}
-    </p>
-  );
-};
-
-export const Selengkap = () => {
-  return (
-    <span className="rounded-2xl font-mono p-2 bg-blue-500 text-white">
-      Selengkapnya
-    </span>
-  );
-};
-
-export default CardProduct;
+export default CardProduct
--- src/Pages/product.jsx
+++ src/Pages/product.jsx
@@ -2,30 +2,26 @@ import CardProduct from "../Components/Fragments/CardProduct";
 
 const ProductPage = () => {
   return (
-    <div className="flex justify-center justify-self-center gap-10">
-    <CardProduct
-      img={"macbook-1.jpg"}
-      title={"MacBook Pro 14” dan 16”"}
-      detailShort={"Chip M4"}
-      detailLong={"Luar biasa tipis dan cepat untuk bekerja, bermain, dan berkarya di mana saja."}
-    />
-    
-     <CardProduct
-      img={"macbook-2.jpg"}
-      title={"MacBook Pro 14” dan 16”"}
-      detailShort={"Chip M5, M4 Pro, atau M4 Max"}
-      detailLong={"Laptop Mac paling canggih untuk pekerjaan berat dengan file besar."}
-    />
+        <div className="flex justify-center gap-3 py-5">
+            <CardProduct>
+                <CardProduct.Header img={'macbook-1.jpg'} ></CardProduct.Header>
+                <CardProduct.Body title={"MacBook Air 13” dan 15”"} detail={"Chip M4"}>Luar biasa tipis dan cepat untuk bekerja, bermain, dan berkarya di mana saja.</CardProduct.Body>
+                <CardProduct.Footer ></CardProduct.Footer>
+            </CardProduct>
 
-    <CardProduct
-      img={"iMac-1.jpg"}
-      title={"iMac"}
-      detailShort={"Chip M4"}
-      detailLong={"Desktop lengkap yang menakjubkan untuk kreativitas dan produktivitas."}
-    />
-    </div>
-    
-  );
-};
+            <CardProduct>
+                <CardProduct.Header img={'macbook-2.jpg'} ></CardProduct.Header>
+                <CardProduct.Body title={"MacBook Pro 14” dan 16”"} detail={"Chip M5, M4 Pro, atau M4 Max"}> Laptop Mac paling canggih untuk pekerjaan berat dengan file besar.</CardProduct.Body>
+                <CardProduct.Footer ></CardProduct.Footer>
+            </CardProduct>
+
+            <CardProduct>
+                <CardProduct.Header img={'iMac-1.jpg'} ></CardProduct.Header>
+                <CardProduct.Body title={"iMac"} detail={"Chip M4"}>Desktop lengkap yang menakjubkan untuk kreativitas dan produktivitas.</CardProduct.Body>
+                <CardProduct.Footer ></CardProduct.Footer>
+            </CardProduct>
+        </div>
+  )
+}
 
 export default ProductPage;
