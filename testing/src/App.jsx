510cbc90bd588029b1695b7522929e9eb709046d
Mipan-Zuzu <kadekisaka00@gmail.com>
2026-02-09 19:11:08 +0800

    feat: useMemo

--- testing/src/App.jsx
+++ testing/src/App.jsx
@@ -1,18 +1,9 @@
-import React, { useState } from 'react'
-import './App.css'
-
-const Child = React.memo(function Child({value}){
-  console.log("render child")
-  return <span>{value}</span>
-})
+import ExampleUseMemo from './Components/ExampleUseMemo'
 
 const App = () => {
-  const [count, setCount] = useState(0)
   return (
     <div>
-        <Child value={"hello"} />
-        <span>{count}</span>
-        <button onClick={() => setCount(count + 1)} >klik</button>
+      <ExampleUseMemo />
     </div>
   )
 }
--- testing/src/Components/Items.jsx
+++ testing/src/Components/Items.jsx
@@ -1,22 +1,8 @@
 import React from 'react'
-import { useContext } from 'react'
-import { ContextItems } from '../context/Context'
 
-const Items = () => {
-    const {items} = useContext(ContextItems)
-  return (
-    <div>
-      <span>barang yg ada di troli kamu : 
-        {items.map((items, index) => {
-          return <div key={index}>
-            <ul>
-              <li>{items}</li>
-            </ul>
-          </div>
-        })}
-      </span>
-    </div>  
-  )
-}
+const Items = React.memo(function Child({value}){
+  console.log("render child")
+  return <span>{value}</span>
+})
 
 export default Items
